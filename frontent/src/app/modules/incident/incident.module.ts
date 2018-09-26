e244dfabd4dc9c19ffc2ecbdd13aff2d99b1883e
Marco Hernandez <marcohern@gmail.com>
2018-09-25 23:08:57 -0500

    map renders propperly with a marker

--- frontent/src/app/modules/incident/incident.module.ts
+++ frontent/src/app/modules/incident/incident.module.ts
@@ -3,9 +3,14 @@ import { CommonModule } from '@angular/common';
 import { AddFormComponent } from './add-form/add-form.component';
 import { MapComponent } from './map/map.component';
 
+import { AgmCoreModule } from '@agm/core';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBV-6ds73oQP_OTbVylTpvhz3rY6RHpOF8'
+    })
   ],
   declarations: [AddFormComponent, MapComponent],
   exports:[ AddFormComponent, MapComponent ]
