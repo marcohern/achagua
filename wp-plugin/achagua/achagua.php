870c1bb9a54c0b8c297f690c91633cda8d110c8c
Marco Hernandez <marcohern@gmail.com>
2018-10-03 00:27:38 -0500

    created plugin

--- wp-plugin/achagua/achagua.php
+++ wp-plugin/achagua/achagua.php
@@ -13,3 +13,26 @@ Author URI: https://marcohern.com
 License: GPLv2 or later
 Text Domain: achagua
 */
+
+//defined(ABSPATH) or die('Access Denied');
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Access Denied';
+	exit;
+}
+
+class Achagua {
+
+    public function activate() {
+        flush_rewrite_rules();
+    }
+
+    public function deactivate() {
+        flush_rewrite_rules();
+    }
+}
+
+$achagua = new Achagua();
+
+register_activation_hook  (__FILE__, [$achagua, 'activate'  ]);
+register_deactivation_hook(__FILE__, [$achagua, 'deactivate']);
+
