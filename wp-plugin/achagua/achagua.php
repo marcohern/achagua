1b8b7957432ed640519479a24bd7cf0db9bf90ea
Marco Hernandez <marcohern@gmail.com>
2018-10-02 21:47:04 -0500

    added Wordpress plugin index

---
+++ wp-plugin/achagua/achagua.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @package Achagua
+ */
+/*
+Plugin Name: Achagua Form
+Plugin URI: https://github.com/marcohern/achagua
+Description: A form for inserting incidents
+Version: 1.0.0
+Author: Marco Hernandez
+Author URI: https://marcohern.com
+License: GPLv2 or later
+Text Domain: achagua
+*/
