bd8d11927c07786ef7e0bd4a115eaea192f91591
Marco Hernandez <marcohern@gmail.com>
2018-10-03 18:35:10 -0500

    added publish and unpublish script

--- publish.bat
+++ publish.bat
@@ -1,1 +1,5 @@
-copy frontend/dist/*.* wp-plugin/achagua/app/
+@echo off
+copy frontent\dist\achagua\*.js .\wp-plugin\achagua\app
+copy frontent\dist\achagua\*.css .\wp-plugin\achagua\app
+copy frontent\dist\achagua\*.ico .\wp-plugin\achagua\app
+copy frontent\dist\achagua\*.txt .\wp-plugin\achagua\app
---
+++ unpublish.bat
@@ -0,0 +1,5 @@
+@echo off
+del .\wp-plugin\achagua\app\*.js
+del .\wp-plugin\achagua\app\*.css
+del .\wp-plugin\achagua\app\*.ico
+del .\wp-plugin\achagua\app\3rd*.txt
