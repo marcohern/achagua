f9aa3d9bf5f05e8451ee2e0cab631b0fb88c026e
Marco Hernandez <marcohern@gmail.com>
2019-02-19 08:13:46 -0500

    published map

--- wp-plugin/achagua/achagua-shortcode.php
+++ wp-plugin/achagua/achagua-shortcode.php
@@ -43,7 +43,7 @@ function achuaga_map_shortcode_scripts() {
 ?>
     <script type="text/javascript" src="/wp-content/plugins/achagua/map/runtime.a66f828dca56eeb90e02.js"></script>
     <script type="text/javascript" src="/wp-content/plugins/achagua/map/polyfills.2f4a59095805af02bd79.js"></script>
-    <script type="text/javascript" src="/wp-content/plugins/achagua/map/main.424ba50ca5dc38c61080.js"></script>
+    <script type="text/javascript" src="/wp-content/plugins/achagua/map/main.d9f39ca4eabac2d4efa8.js"></script>
 <?php
 }
 
